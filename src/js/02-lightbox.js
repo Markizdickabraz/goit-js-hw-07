e66876a30a1c9223afcd847fa6aacfbb19a7a52f
Sakhatskiy Mark Igorevich <110189603+Markizdickabraz@users.noreply.github.com>
2022-11-29 21:02:04 +0200

    2 частина ДЗ

--- src/js/02-lightbox.js
+++ src/js/02-lightbox.js
@@ -1,4 +1,35 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+
+
+const gallery = document.querySelector('.gallery');
+// console.log(gallery);
+
+const makeGalleryItems = ({preview, original, description}) => {
+    return `
+  
+  <a class="gallery__item" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      alt="${description}"
+    />
+  </a>
+    `;
+};
+
+// додаю галерею в DOM
+
+const galleryList = galleryItems.map(makeGalleryItems).join(' ');
+// console.log(galleryList);
+gallery.innerHTML = galleryList;
+
+let galleryOpenModal = new SimpleLightbox('.gallery a', {captionsData: "alt", captionDelay : 250});
+galleryOpenModal.on('show.simplelightbox', function () {
+});
+
+
+galleryOpenModal.on('error.simplelightbox', function (e) {
+	console.log(e); // some usefull information
+});
